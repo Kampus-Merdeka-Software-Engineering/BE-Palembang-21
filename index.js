ac2c477c70ac4976a2c13ae3e49b271644f7444e
dennys <99785104+dennysdhmra@users.noreply.github.com>
2023-10-09 14:01:56 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import sequelize from "./config/sequelize.js";
+import userRoutes from "./routes/users.js";
+import { startSequelize } from "./utils/startSequelize.js";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware untuk mengizinkan aplikasi menerima JSON
+app.use(express.json());    
+app.use(bodyParser.json());
+app.use(cors());
+
+// Gunakan rute pengguna
+app.use('/new', userRoutes);
+app.use('/', userRoutes);
+
+startSequelize(sequelize);
+
+// Post Server
+const port = process.env.PORT || 3000;
+
+sequelize.authenticate().then(() => {
+    console.log("Database connection established!");
+});
+
+app.listen(port, () => {
+    console.log('server is running at port ${port}');
+});
